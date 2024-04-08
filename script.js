eb9364c378874aed1ee161eeca77f7da39d8ce50
buciamichal1 <buciamichal@gmail.com>
2024-04-08 21:37:54 +0200

    Dodanie podkategori i ich wystylizowanie oraz funkcjonalność

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 $(function(){
-    x=0;
+    x = 0;
     $("#menu").click(function() {
         if (x == 0) {
-          $('.categories').css({'visibility':'visible','display':'block'});
-          
-          $('.categories').stop().animate({'width': '30vh'}, 500);
-          x = 1;
+            $('.categories').css({'visibility':'visible','display':'block'});
+            $('.categories').stop().animate({'width': '30vh'}, 500);
+            x = 1;
         } else {
-          $('.categories').stop().animate({'width': '0vh'}, 500,function(){
+            $('.categories').stop().animate({'width': '0vh'}, 500,function(){
             $('.categories').css({'visibility':'hidden','display':'none'});});
-          
-          x = 0;
+            x = 0;
         }
       });
-    
-    });
+    $(".category").click(function() {
+        var category = $(this).data('category'); // Pobierz wartość atrybutu data-category klikniętej kategorii
+        var subcategories = $('.subcategories[data-category="' + category + '"]'); // Znajdź podkategorie odpowiadające kategorii
+        var subcategory = $('.subcategory[data-category="' + category + '"]'); 
+        if (subcategories.css('display') == 'none') {
+            $(".subcategory").not(subcategories).stop().animate({'height': '0vh'}, 500,function(){
+            $(".subcategories").not(subcategories).css({'display':'none'});}); // Ukryj wszystkie podkategorie
+            subcategories.css({'display':'block'});
+            subcategory.stop().animate({'height': '5vh'}, 500);
+        } else {
+            subcategory.stop().animate({'height': '0vh'}, 500,function(){
+            subcategories.css({'display':'none'});});
+        }
+      });
+});
+
